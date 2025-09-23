3e5b3a8bd5208ad84cebfb2c9fc94bd05d2b3b74
Egor Opekin <vip.opekin@mail.ru>
2025-09-23 18:23:08 +0600

    chore: initial commit

---
+++ src/database.py
@@ -0,0 +1,35 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import DeclarativeBase
+
+from src.config import settings
+
+
+engine = create_async_engine(
+    url=settings.ASYNC_DATABASE_URL,
+    echo=True,
+    echo_pool=True,
+    future=True,
+    pool_size=5,
+    max_overflow=10
+)
+
+async_session_maker = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+class Base(DeclarativeBase):
+    pass
